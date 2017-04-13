0a40f91ce3a1ed94f5431caea58fa1df17595b95
yangyuemei <895191720@qq.com>
2017-04-13 15:43:55 +0800

    add test/learngit/hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+
+
+print('hello world')
+Administrator@BZB19 MINGW64 ~/test/learngit (dev)
+$ git push -u origin dev
+Administrator@BZB19 MINGW64 ~/test/learngit (dev)
+$ git push -u origin dev
+Administrator@BZB19 MINGW64 ~/test/learngit (dev)
+$ git push -u origin dev
